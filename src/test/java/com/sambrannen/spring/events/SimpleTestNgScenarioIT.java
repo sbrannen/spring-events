9ff329a01617f79cde7ea96979c15393d8936e6c
Sam Brannen <sam@sambrannen.com>
2015-09-29 01:19:42 +0200

    Upgrade to AssertJ 3.2.0

--- src/test/java/com/sambrannen/spring/events/SimpleTestNgScenarioIT.java
+++ src/test/java/com/sambrannen/spring/events/SimpleTestNgScenarioIT.java
@@ -16,7 +16,7 @@
 
 package com.sambrannen.spring.events;
 
-import static org.assertj.core.api.StrictAssertions.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
