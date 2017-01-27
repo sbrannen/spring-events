dfbd35650fe80bbf6f664238d4c4febdfce30d43
Sam Brannen <sam@sambrannen.com>
2017-01-27 05:09:35 +0100

    Add httpie examples

--- src/test/java/com/sambrannen/spring/events/web/RestEventsControllerTests.java
+++ src/test/java/com/sambrannen/spring/events/web/RestEventsControllerTests.java
@@ -95,9 +95,12 @@ class RestEventsControllerTests {
 	@Test
 	@WithMockUser(roles = "ADMIN")
 	void updateEvent() throws Exception {
+
+		String json = "{\"eventDate\": \"2015-04-30\", \"name\": \"Edited\", \"location\": \"Integration Test\"}";
+
 		mockMvc.perform(
 			put("/events/{id}", 9).contentType(APPLICATION_JSON)
-				.content("{\"name\": \"Edited\", \"location\": \"Integration Test\"}"))//
+					.content(json))//
 			.andExpect(status().isNoContent());
 
 		Event updatedEvent = repo.findOne(9L);
@@ -128,7 +131,28 @@ class RestEventsControllerTests {
 // {"id":7,"eventDate":"2014-09-10","name":"SpringOne 2GX","location":"Dallas"},
 // {"id":8,"eventDate":"2014-11-06","name":"Spring eXchange","location":"London"},
 // {"id":9,"eventDate":"2015-04-30","name":"Spring I/O","location":"Barcelona"}
+// {"id":10,"eventDate":"2015-09-15","name":"SpringOne 2GX","location":"Washington, D.C."},
+// {"id":11,"eventDate":"2016-05-19","name":"Spring I/O","location":"Barcelona"},
+// {"id":12,"eventDate":"2016-08-03","name":"SpringOne Platform","location":"Las Vegas"}
 // ]
+
+//-----------------------------------------------------------------------------
+//--- httpie
+//-----------------------------------------------------------------------------
+
+// http GET http://localhost:8080/events
+
+// http GET http://localhost:8080/events/9
+
+// http -a admin:test POST http://localhost:8080/events/ name=Spring! location='Command Line'
+
+// http -a admin:test PUT http://localhost:8080/events/9 eventDate=2015-04-30 name=Edited location='Command Line'
+
+// http -a admin:test DELETE http://localhost:8080/events/9
+
+//-----------------------------------------------------------------------------
+//--- curl and json_pp
+//-----------------------------------------------------------------------------
 
 // curl -H "Accept:application/json" http://localhost:8080/events | json_pp
 
@@ -136,6 +160,6 @@ class RestEventsControllerTests {
 
 // curl -u admin:test -i -X POST -H "Content-Type:application/json" http://localhost:8080/events/ -d '{"name": "Spring!", "location": "Command Line"}'
 
-// curl -u admin:test -i -X PUT -H "Content-Type:application/json" http://localhost:8080/events/9 -d '{"name": "Edited", "location": "Command Line"}'
+// curl -u admin:test -i -X PUT -H "Content-Type:application/json" http://localhost:8080/events/9 -d '{"eventDate":"2015-04-30", "name": "Edited", "location": "Command Line"}'
 
 // curl -u admin:test -i -X DELETE http://localhost:8080/events/9
