07c85104ccf414a6c462fa1c2fbfcc8201429338
Sam Brannen <sbrannen@pivotal.io>
2019-07-16 18:49:41 +0200

    Polishing

--- src/test/java/com/sambrannen/spring/events/ApplicationTests.java
+++ src/test/java/com/sambrannen/spring/events/ApplicationTests.java
@@ -20,16 +20,12 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * @author Sam Brannen
  * @since 1.0
  */
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = DEFINED_PORT)
 @Disabled
 class ApplicationTests {
